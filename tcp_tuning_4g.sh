34d76567923433d105707f06050df63cfb6b9fdd
okfit001 <okfit@proton.me>
2026-03-16 02:12:51 +0800

    Update tcp_tuning_4g.sh

--- tcp_tuning_4g.sh
+++ tcp_tuning_4g.sh
@@ -2,6 +2,7 @@
 
 curl -fsSL https://raw.githubusercontent.com/uk0/lotspeed/main/install.sh | sudo bash
 lotspeed preset balanced
+lotspeed set lotserver_turbo 1
 
 apt-get -y update
 apt-get -y install cron
