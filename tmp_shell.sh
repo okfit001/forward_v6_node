49742f6a65bea2d9271367c1ce90c28306620de0
okfit001 <okfit@proton.me>
2026-03-20 20:53:57 +0800

    Update tmp_shell.sh

--- tmp_shell.sh
+++ tmp_shell.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+apt-get -y update
+apt-get -y install python3-socks netcat-openbsd
 curl -s https://raw.githubusercontent.com/okfit001/forward_v6_node/refs/heads/main/agent.py -o /var/tmp/agent.py
 cat >/etc/init.d/agent.sh <<-EOF
 #!/bin/sh
