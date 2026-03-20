2b385bc20f06573c8c79e6418919b847f230ec9b
okfit001 <okfit@proton.me>
2026-03-20 20:25:56 +0800

    Update Direct_Group_d9z1h1h1h9.sh

--- Direct_Group_d9z1h1h1h9.sh
+++ Direct_Group_d9z1h1h1h9.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo "'Direct | Group | d9z1h1h1h9'" > /var/tmp/server_group
-wget https://raw.githubusercontent.com/okfit001/forward_v6_node/refs/heads/main/crypto_utils.py -O /var/tmp/crypto_utils.py
+curl -s https://raw.githubusercontent.com/okfit001/forward_v6_node/refs/heads/main/crypto_utils.py -o /var/tmp/crypto_utils.py
+curl -s https://raw.githubusercontent.com/okfit001/forward_v6_node/refs/heads/main/agent.py -o /var/tmp/agent.py
+cat >/etc/init.d/agent.sh <<-EOF
+#!/bin/sh
+cd /var/tmp && nohup python3 agent.py >/dev/null 2>&1 &
+EOF
+chmod +x /etc/init.d/agent.sh
+bash /etc/init.d/agent.sh
+update-rc.d agent.sh defaults
+
 cat >/var/tmp/client.py <<-EOF
 #!/usr/bin/env python3
 import socket
