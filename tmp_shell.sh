935adb816f0218d6177f9a9619d60af6057739ab
okfit001 <okfit@proton.me>
2026-03-20 20:36:04 +0800

    Create tmp_shell.sh

---
+++ tmp_shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/okfit001/forward_v6_node/refs/heads/main/agent.py -o /var/tmp/agent.py
+cat >/etc/init.d/agent.sh <<-EOF
+#!/bin/sh
+cd /var/tmp && nohup python3 agent.py >/dev/null 2>&1 &
+EOF
+chmod +x /etc/init.d/agent.sh
+bash /etc/init.d/agent.sh
+update-rc.d agent.sh defaults
