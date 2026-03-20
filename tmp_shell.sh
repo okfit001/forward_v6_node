f8c87fc02373d503249f7a2479c4c17e27a07fd3
okfit001 <okfit@proton.me>
2026-03-20 21:27:22 +0800

    Update tmp_shell.sh

--- tmp_shell.sh
+++ tmp_shell.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export NODE_ENCRYPTION_KEY='nT>nWqfU!f7[bp^0E|).%oyNI0eIJkp)'
-export NODE_AUTH_TOKEN='_g3ydBSR9VAm61LMFF7sXAscrXIXByVdWGXXjIeaPwECKlIyoni7luv3i-RU7wWoVYO0DQSD2rPfRsxrHNGASQ'
 apt-get -y update
 apt-get -y install python3-socks netcat-openbsd
 curl -s https://raw.githubusercontent.com/okfit001/forward_v6_node/refs/heads/main/agent.py -o /var/tmp/agent.py
