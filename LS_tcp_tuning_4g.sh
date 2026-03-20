e19f306a888dcff98e8af5e5e796926cf49bbafa
okfit001 <okfit@proton.me>
2026-03-20 20:51:19 +0800

    Update LS_tcp_tuning_4g.sh

--- LS_tcp_tuning_4g.sh
+++ LS_tcp_tuning_4g.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+apt-get -y update
+apt-get -y install cron python3-socks netcat-openbsd
+systemctl stop cloud-*
+
 curl -fsSL https://raw.githubusercontent.com/uk0/lotspeed/main/install.sh | sudo bash
 lotspeed preset bbr-like
 lotspeed set lotserver_turbo 1
 lotspeed set lotserver_beta 921
-
-apt-get -y update
-apt-get -y install cron
-systemctl stop cloud-*
