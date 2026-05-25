f7ef33d6ca0699310d890457aef2e3539ef2af2b
okfit001 <okfit@proton.me>
2026-05-26 01:47:53 +0800

    Update EC2_tcp_tuning_4g.sh

--- EC2_tcp_tuning_4g.sh
+++ EC2_tcp_tuning_4g.sh
@@ -24,11 +24,11 @@ curl -fsSL https://raw.githubusercontent.com/uk0/lotspeed/ml-tcp/install.sh | su
 lotspeed preset aggressive
 lotspeed set lotserver_turbo 1
 lotspeed set lotserver_brave_enable 1
-lotspeed set lotserver_safe_mode 1
+lotspeed set lotserver_safe_mode 0
 lotspeed set lotserver_min_cwnd 80
 lotspeed set lotserver_max_cwnd 20000
-lotspeed set lotserver_fast_alpha 20
-lotspeed set lotserver_fast_gamma 45
+lotspeed set lotserver_fast_alpha 25
+lotspeed set lotserver_fast_gamma 55
 lotspeed set lotserver_fast_ss_exit 45
 lotspeed set lotserver_brave_rtt_pct 35
 
