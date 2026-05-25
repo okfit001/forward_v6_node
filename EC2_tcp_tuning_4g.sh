3ad514f604fb6890bc9059b16d2d0998190e0972
okfit001 <okfit@proton.me>
2026-05-25 16:29:06 +0800

    Update and rename tcp_tuning_4g.sh to EC2_tcp_tuning_4g.sh

---
+++ EC2_tcp_tuning_4g.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y install cron python3-socks netcat-openbsd
+systemctl stop cloud-*
+
+cat > /etc/sysctl.conf << EOF
+fs.file-max = 6815744
+vm.min_free_kbytes = 1048576
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.core.rmem_max=10000000
+net.core.wmem_max=10000000
+net.ipv4.tcp_rmem=4096 190054 10000000
+net.ipv4.tcp_wmem=4096 190054 10000000
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+sysctl -p && sysctl --system
