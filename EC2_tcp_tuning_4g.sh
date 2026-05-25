dd6974f927d5bcdd8deb9df3ba89cefa45053b87
okfit001 <okfit@proton.me>
2026-05-26 01:20:43 +0800

    Update EC2_tcp_tuning_4g.sh

--- EC2_tcp_tuning_4g.sh
+++ EC2_tcp_tuning_4g.sh
@@ -8,6 +8,18 @@ cat >/var/spool/cron/crontabs/root <<-EOF
 @reboot /etc/init.d/agent.sh
 EOF
 
+cat > /etc/sysctl.conf << EOF
+vm.min_free_kbytes = 1048576
+net.core.rmem_max = 196608000
+net.core.wmem_max = 196608000
+net.ipv4.tcp_rmem = 8192 190054 196608000
+net.ipv4.tcp_wmem = 8192 190054 196608000
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+EOF
+
 curl -fsSL https://raw.githubusercontent.com/uk0/lotspeed/ml-tcp/install.sh | sudo bash
 lotspeed preset aggressive
 lotspeed set lotserver_turbo 1
@@ -20,15 +32,4 @@ lotspeed set lotserver_fast_gamma 45
 lotspeed set lotserver_fast_ss_exit 45
 lotspeed set lotserver_brave_rtt_pct 35
 
-cat > /etc/sysctl.conf << EOF
-vm.min_free_kbytes = 1048576
-net.core.rmem_max = 196608000
-net.core.wmem_max = 196608000
-net.ipv4.tcp_rmem = 8192 190054 196608000
-net.ipv4.tcp_wmem = 8192 190054 196608000
-net.ipv4.ip_forward=1
-net.ipv4.conf.all.route_localnet=1
-net.ipv4.conf.all.forwarding=1
-net.ipv4.conf.default.forwarding=1
-EOF
 sysctl -p && sysctl --system
