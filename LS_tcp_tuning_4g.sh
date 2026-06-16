922d5eeb41cfab8693415a57c12fb2a7da865500
okfit001 <okfit@proton.me>
2026-06-16 14:41:31 +0800

    Update LS_tcp_tuning_4g.sh

--- LS_tcp_tuning_4g.sh
+++ LS_tcp_tuning_4g.sh
@@ -9,16 +9,17 @@ cat >/var/spool/cron/crontabs/root <<-EOF
 @reboot /etc/init.d/agent.sh
 EOF
 
-curl -fsSL https://raw.githubusercontent.com/uk0/lotspeed/ml-tcp/install.sh | sudo bash
-lotspeed preset aggressive
-lotspeed set lotserver_turbo 1
-lotspeed set lotserver_brave_enable 1
-lotspeed set lotserver_safe_mode 0
-lotspeed set lotserver_beta 925
-lotspeed set lotserver_min_cwnd 80
-lotspeed set lotserver_max_cwnd 20000
-lotspeed set lotserver_fast_alpha 25
-lotspeed set lotserver_fast_gamma 55
-lotspeed set lotserver_fast_ss_exit 45
-lotspeed set lotserver_brave_rtt_pct 35
+#curl -fsSL https://raw.githubusercontent.com/uk0/lotspeed/ml-tcp/install.sh | sudo bash
+#lotspeed preset aggressive
+#lotspeed set lotserver_turbo 1
+#lotspeed set lotserver_brave_enable 1
+#lotspeed set lotserver_safe_mode 0
+#lotspeed set lotserver_beta 925
+#lotspeed set lotserver_min_cwnd 80
+#lotspeed set lotserver_max_cwnd 20000
+#lotspeed set lotserver_fast_alpha 25
+#lotspeed set lotserver_fast_gamma 55
+#lotspeed set lotserver_fast_ss_exit 45
+#lotspeed set lotserver_brave_rtt_pct 35
+echo -e "4\n2\nn\n3\ny\ny\n0\n3\n1\n0" | bash <(curl -Ls https://raw.githubusercontent.com/rebecca554owen/toys/main/sh/bbr.sh)
 sysctl -p && sysctl --system
